47bf8c6c092da9876ac6570d94a2ee393b560b29
Lucas Correia <lucascorreia-.-@live.com>
2018-12-19 21:23:14 -0200

    Updates

---
+++ precache-manifest.75b5d11dbc7886e436542fe33e46b7a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/GitHubApp/static/media/github-mark1.3e54ed15.png"
+  },
+  {
+    "revision": "b625e0bb5c591734c682",
+    "url": "/GitHubApp/static/js/runtime~main.b625e0bb.js"
+  },
+  {
+    "revision": "9942305e4c17cde96560",
+    "url": "/GitHubApp/static/js/main.9942305e.chunk.js"
+  },
+  {
+    "revision": "d6d860f780d350f0d3cd",
+    "url": "/GitHubApp/static/js/1.d6d860f7.chunk.js"
+  },
+  {
+    "revision": "9942305e4c17cde96560",
+    "url": "/GitHubApp/static/css/main.16bddc03.chunk.css"
+  },
+  {
+    "revision": "125b278ac3d4bda4a9bdebc0a529ff9f",
+    "url": "/GitHubApp/index.html"
+  }
+];
