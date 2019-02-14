cf5d71276f0e0db685c1a2efab2c35c73e0a96b3
Lucas Correia <lucascorreia-.-@live.com>
2019-02-14 19:25:39 -0200

    ajuste link rodape

--- src/template/Footer.jsx
+++ src/template/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => (
     <div className="footer">
-        <span>App desenvolvido por <a href="www.linkedin.com/in/lucascorreia-95">Lucas Correia</a> em 11/2018</span>
+        <span>App desenvolvido por <a href="https://www.linkedin.com/in/lucascorreia-95">Lucas Correia</a> em 11/2018</span>
     </div>
 )
